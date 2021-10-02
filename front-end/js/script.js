0dc0711ec5d523a24c03733f971872d81d8fb4ba
kahilkubilay <kahilkubilay@gmail.com>
2021-10-02 23:20:20 +0300

    folders edited and first js task loaded

---
+++ front-end/js/script.js
@@ -0,0 +1,21 @@
+let user = prompt('İsminizi bahşeder misiniz');
+let date = new Date();
+let utcDay = date.getDay();
+let day;
+let days = ['Pazartesi', 'Salı', 'Çarşamba', 'Perşembe', 'Cuma', 'Cumartesi', 'Pazar']
+
+document.querySelector('.user').innerHTML += `${user[0].toUpperCase() + user.slice(1)}`;
+
+setTime();
+
+function setTime() {
+    let updateDate = new Date();
+
+    document.querySelector('.time>h1').innerHTML = `
+        ${updateDate.getHours()}
+        :${updateDate.getMinutes()}:
+        ${updateDate.getSeconds()} 
+        ${days[updateDate.getDay() - 1]}`;
+}
+
+setInterval(setTime, 1000);
