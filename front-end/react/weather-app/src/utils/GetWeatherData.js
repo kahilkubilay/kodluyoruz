d5c323f97adc97045396845f5bef29456425f250
kahilkubilay <kahilkubilay@gmail.com>
2021-12-12 23:16:13 +0300

    fx

--- front-end/react/weather-app/src/utils/GetWeatherData.js
+++ front-end/react/weather-app/src/utils/GetWeatherData.js
@@ -11,9 +11,6 @@
 //         lang: ''
 //     });
 
-//     // key-list:: 0650a12d4ab140b28d0215630210912 8bf0804f48c90c059ffee4d9bf414e23 6969c4badf0c957576a52d647746cb7a
-//     // const key = '826b82528457cde37c96c39e1602baec';
-//     const key = '';
 
 //     useEffect(() => {
 //         fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=37.00167&lon=35.32889&exclude=current,hourly,minutely,alerts&units=metric&appid=${key}`)
