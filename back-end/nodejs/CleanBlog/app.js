e6a90f0f66dc9d9461a004099340ba16aca34e6c
kahilkubilay <kahil_kubilay@hotmail.com>
2023-05-07 15:05:16 +0300

    added ejs module

--- back-end/nodejs/CleanBlog/app.js
+++ back-end/nodejs/CleanBlog/app.js
@@ -1,16 +1,31 @@
 import express from 'express';
+import ejs from 'ejs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    const blog = {
-        id: 1,
-        title: 'Blog Title',
-        description: 'Blog Description'
-    }
+// template engine
+app.set('view engine', 'ejs');
 
-    res.send(blog)
+// middlewares
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+app.get('/add_post', (req, res) => {
+  res.render('add_post')
+})
+app.get('/about', (req, res) => {
+  res.render('about')
+})
+app.get('/post', (req, res) => {
+  res.render('post')
 })
 
 app.listen(port, () => {
